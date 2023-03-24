30e20af764a30c6038239f0decf8e34255c17bba
keraemoe <weekera@gmail.com>
2023-03-25 01:42:28 +0600

    add

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                 <div className="max-w-2xl mx-auto text-white py-10">
                     <div className="text-center">
                         <h3 className="text-3xl mb-3">
-                            <AnimatedLast text={"Download our app "} />
+                            <AnimatedLast text={"Fucking Slave "} />
                         </h3>
                         <h4>
                             <AnimatedLast text={"Lorem ipsum, dolor sit amet consectetur adipisicing elit."} />
@@ -23,29 +23,16 @@ const Footer = () => {
                                 hidden: { opacity: 0, scale: 0 },
                             }}
                             className="flex justify-center my-10">
-                            <div className="flex items-center border w-auto rounded-lg px-4 py-2 mx-2">
-                                <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" className="w-7 md:w-8" />
-                                <div className="text-left ml-3">
-                                    <p className='text-xs text-gray-200'>Download on </p>
-                                    <p className="text-sm md:text-base"> Google Play Store </p>
-                                </div>
-                            </div>
-                            <div className="flex items-center border w-auto rounded-lg px-4 py-2 mx-2">
-                                <img src="https://cdn-icons-png.flaticon.com/512/888/888841.png" className="w-7 md:w-8" />
-                                <div className="text-left ml-3">
-                                    <p className='text-xs text-gray-200'>Download on </p>
-                                    <p className="text-sm md:text-base"> Apple Store </p>
-                                </div>
-                            </div>
                         </motion.div>
                     </div>
                     <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
                         <p className="order-2 md:order-1 mt-8 md:mt-0"> &copy; Fucking Slave, 2025 </p>
                         <div className="order-1 md:order-2">
                             <a href="#" className='px-2'>Home</a>
-                            <a href="#app" className='px-2 border-l'>The App</a>
+                            <a href="#info" className='px-2 border-l'>Info</a>
                             <a href="#features" className='px-2 border-l'>Features</a>
-                            <a href="#contact" className='px-2 border-l'>Contact</a>
+                            <a href="#projects" className='px-2 border-l'>Projects</a>
+                            <a href="#contact" className='px-2 border-l'>Contacts</a>
                         </div>
                     </div>
                 </div>
